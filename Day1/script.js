ba7e89689b55430a789ea636edd954a8fd12c060
bourbiafawzi <fawzibourbia5@gmail.com>
2025-12-20 02:31:14 +0100

    Implement calculator operations and result display (Calculator Project)

--- Day1/script.js
+++ Day1/script.js
@@ -2,21 +2,21 @@ const buttonsNumber = document.querySelectorAll(".btn-number");
 const buttonsOperator = document.querySelectorAll(".btn-operator");
 const buttonsSystem = document.querySelectorAll(".btn-system");
 const containerResult = document.querySelector(".container-result");
-let numberA;
-let numberB;
-let operator;
+const equal = document.querySelector(".btn-equal");
+let text;
 
-console.log(buttonsOperator, buttonsNumber);
-
+// click number button event
 for (let i = 0; i < buttonsNumber.length; i++) {
   buttonsNumber[i].addEventListener("click", () => {
     if (containerResult.value == 0) containerResult.value = "";
 
     containerResult.value =
       containerResult.value + buttonsNumber[i].textContent;
+    text = containerResult.value;
+    divider(text);
   });
 }
-
+// click operator button event
 for (let i = 0; i < buttonsOperator.length; i++) {
   buttonsOperator[i].addEventListener("click", () => functionOperator(i));
 }
@@ -25,7 +25,7 @@ for (let i = 0; i < buttonsOperator.length; i++) {
 for (let i = 0; i < buttonsSystem.length; i++) {
   buttonsSystem[i].addEventListener("click", () => functionSystem(i));
 }
-
+//function that manage the operator + - * /
 function functionOperator(i) {
   if (containerResult.value != 0) {
     if (
@@ -34,40 +34,68 @@ function functionOperator(i) {
       containerResult.value[containerResult.value.length - 1] === "/" ||
       containerResult.value[containerResult.value.length - 1] === "*"
     ) {
-      numberA = Number(containerResult.value);
-      operator = buttonsOperator[i].textContent;
       containerResult.value =
         containerResult.value.slice(0, -1) + buttonsOperator[i].textContent;
+      text = containerResult.value;
+      divider(text);
     } else {
       containerResult.value =
         containerResult.value + buttonsOperator[i].textContent;
-      numberA = Number(containerResult.value);
-      operator = buttonsOperator[i].textContent;
+      text = containerResult.value;
+      divider(text);
     }
   }
 }
-
+// function that manage the buttons C CE
 function functionSystem(i) {
   console.log(buttonsSystem[i].textContent);
   if (buttonsSystem[i].textContent === "C") {
     containerResult.value = "";
-    numberA = undefined;
-    numberB = undefined;
-  } else if (buttonsSystem[i].textContent === "CE") {
-    if (typeof numberA === "undefined") containerResult.value = "";
-    else {
-      numberB = undefined;
-      containerResult.value = numberA;
-    }
   }
-  // it stayed the operator -/+
+}
 
-  //THE OPERATOR EQUAL
-  else if (buttonsSystem[i].textContent === "=") {
-    if (typeof numberA === "undefined")
-      containerResult.value = containerResult.value;
-    else if (typeof numberA != "undefined" && typeof numberB === "undefined")
-      containerResult.value = containerResult.value;
-    else containerResult.value = numberA + numberB; // changer ca le resultat est suivant l operateur
+//THE OPERATOR EQUAL
+equal.addEventListener("click", () => {
+  if (result(divider(containerResult.value))) {
+    containerResult.value = result(divider(containerResult.value));
   }
+});
+
+//function that divide the container
+function divider(t) {
+  if (/.+[+\-*/].+/.test(t)) {
+    const result = t.match(/(.+?)([+\-*/])(.+)/);
+    console.log(result[1], result[2], result[3]);
+
+    return {
+      avant: result[1],
+      operator: result[2],
+      apres: result[3],
+    };
+  } else if (!/[+\-*/]/.test(t) && t != "")
+    return {
+      avant: t,
+      operator: null,
+      apres: null,
+    };
+  else if (/[+\-*/]$/.test(t))
+    return {
+      avant: t.slice(0, -1),
+      operator: null,
+      apres: null,
+    };
+  else return false;
+}
+//function calculate result
+
+function result(o) {
+  let result;
+  if (o && o.operator != null) {
+    if (o.operator == "+") result = Number(o.avant) + Number(o.apres);
+    else if (o.operator == "-") result = Number(o.avant) - Number(o.apres);
+    else if (o.operator == "/") result = Number(o.avant) / Number(o.apres);
+    else if (o.operator == "*") result = Number(o.avant) * Number(o.apres);
+    return result;
+  } else if (o && o.operator === null) return o.avant;
+  else return false;
 }
